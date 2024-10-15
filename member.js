b2143a7591045d55d9d469d89c44ba6c61ad642b
Facundo Gayoso <87102414+facusbrr@users.noreply.github.com>
2024-10-15 14:51:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,126 @@
+function skillsMember() {
+  return {
+    skills: [
+      {
+        name: "JavaScript",
+        level: "Advanced"
+      },
+      {
+        name: "HTML",
+        level: "Advanced"
+      },
+      {
+        name: "CSS",
+        level: "Advanced"
+      },
+      {
+        name: "Python",
+        level: "Intermediate"
+      },
+      {
+        name: "React",
+        level: "Intermediate"
+      },
+      {
+        name: "NodeJS",
+        level: "Intermediate"
+      },
+      {
+        name: "ExpressJS",
+        level: "Intermediate"
+      },
+      {
+        name: "MongoDB",
+        level: "Intermediate"
+      },
+      {
+        name: "Java",
+        level: "Intermediate"
+      },
+      {
+        name: "C++",
+        level: "Intermediate"
+      },
+      {
+        name: "SQL",
+        level: "Intermediate"
+      },
+      {
+        name: "Django",
+        level: "Intermediate"
+      },
+      {
+        name: "Ruby",
+        level: "Intermediate"
+      },
+      {
+        name: "C#",
+        level: "Intermediate"
+      },
+      {
+        name: "PHP",
+        level: "Intermediate"
+      },
+      {
+        name: "Angular",
+        level: "Intermediate"
+      },
+      {
+        name: "Vue",
+        level: "Intermediate"
+      },
+      {
+        name: "Bootstrap",
+        level: "Intermediate"
+      },
+      {
+        name: "Sass",
+        level: "Intermediate"
+      },
+      {
+        name: "Less",
+        level: "Intermediate"
+      },
+      {
+        name: "Materialize",
+        level: "Intermediate"
+      },
+      {
+        name: "Material-UI",
+        level: "Intermediate"
+      },
+      {
+        name: "JQuery",
+        level: "Intermediate"
+      },
+      {
+        name: "TypeScript",
+        level: "Intermediate"
+      },
+      {
+        name: "Redux",
+        level: "Intermediate"
+      },
+      {
+        name: "D3",
+        level: "Intermediate"
+      },
+      {
+        name: "Three.js",
+        level: "Intermediate"
+      },
+      {
+        name: "WebGL",
+        level: "Intermediate"
+      },
+      {
+        name: "OpenGL",
+        level: "Intermediate"
+      },
+      {
+        name: "Unity",
+        level: "Intermediate"
+      }
+    ]
+  };
+}
